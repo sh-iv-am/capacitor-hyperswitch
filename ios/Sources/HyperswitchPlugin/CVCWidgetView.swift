9ff04bea6c10c8f4c603bf394566c08abb53972d
25harsh <harshit.srivastava@juspay.in>
2026-04-29 17:32:25 +0530

    fix: ios widget name

---
+++ ios/Sources/HyperswitchPlugin/CVCWidgetView.swift
@@ -0,0 +1,23 @@
+import Foundation
+import Hyperswitch
+import UIKit
+
+public final class CVCWidgetContainer: UIView {
+    private(set) var widget: CVCWidget?
+
+    @discardableResult
+    func attach(paymentSession: PaymentSession, configuration: [String: Any] = [:]) -> CVCWidget {
+        if let existing = widget { return existing }
+        let widget = CVCWidget(paymentSession: paymentSession, configuration: configuration)
+        widget.frame = bounds
+        widget.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        addSubview(widget)
+        self.widget = widget
+        return widget
+    }
+
+    func detach() {
+        widget?.removeFromSuperview()
+        widget = nil
+    }
+}
